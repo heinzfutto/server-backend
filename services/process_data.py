4c9a8b06528832c07047ddd3e61a3af88e9596f3
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2018-08-03 16:11:33 -0400

    Chages

--- services/process_data.py
+++ services/process_data.py
@@ -1,3 +1,9 @@
+# modify python path so that this script can be targeted directly but still import everything.
+import imp as _imp
+from os.path import abspath as _abspath
+_current_folder_init = _abspath(__file__).rsplit('/', 1)[0]+ "/__init__.py"
+_imp.load_source("__init__", _current_folder_init)
+
 from libs.file_processing import process_file_chunks
 
 if __name__ == "__main__":
