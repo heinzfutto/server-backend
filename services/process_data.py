0cb6f81b92f8662cae72cbf35efef7ba88c1197b
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2018-08-03 16:26:52 -0400

    Added log

--- services/process_data.py
+++ services/process_data.py
@@ -3,8 +3,10 @@ import imp as _imp
 from os.path import abspath as _abspath
 _current_folder_init = _abspath(__file__).rsplit('/', 1)[0]+ "/__init__.py"
 _imp.load_source("__init__", _current_folder_init)
+import datetime
 
 from libs.file_processing import process_file_chunks
 
 if __name__ == "__main__":
+	print("Running at: " + datetime.datetime.now())
 	process_file_chunks()
