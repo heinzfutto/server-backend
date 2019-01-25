d906b8290acaf4aeb3706c77d363fff59e13a41a
LexiLuo <631221417@qq.com>
2019-01-24 22:10:23 -0500

    Update mobile_api.py
    1

--- api/mobile_api.py
+++ api/mobile_api.py
@@ -323,6 +323,7 @@ def get_study_params(OS_API=""):
 @mobile_api.route('/login_user', methods=['GET', 'POST'])
 @mobile_api.route('/login_user/ios/', methods=['GET', 'POST'])
 @determine_os_api
+@authenticate_user_registration
 def login_user(OS_API=""):
     patient_id = request.values['patient_id']
     phone_number = request.values['phone_number']
